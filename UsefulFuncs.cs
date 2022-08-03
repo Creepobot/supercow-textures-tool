70ad65df09285355ca3c6da80a87d47d879d5871
Creepobot <61410619+Creepobot@users.noreply.github.com>
2022-08-03 18:17:59 +0300

    Add comments or smth to UsefulFuncs.cs

--- UsefulFuncs.cs
+++ UsefulFuncs.cs
@@ -3,8 +3,13 @@ using System.IO;
 
 public static class UsefulFuncsClass
 {
+    /// <summary>Just overused byte array</summary>
     public static byte[] FFD9 = new byte[] { 0xFF, 0xD9 };
 
+    /// <summary>Finds bytes in a byte array</summary>
+    /// <param name="src">Byte array to search</param>
+    /// <param name="find">Byte array to be found</param>
+    /// <returns>Byte index</returns>
     public static int FindBytes(this byte[] src, byte[] find)
     {
         int index = -1;
@@ -28,7 +33,11 @@ public static class UsefulFuncsClass
         return index;
     }
 
-    public static string TGATag(this string filepath, bool isRemove = false)
+    /// <summary>Adds or removes "_a_" from the beginning of the filename</summary>
+    /// <param name="filepath">Path to the desired file</param>
+    /// <param name="isRemove">Need to add "_a_" or remove?</param>
+    /// <returns>Path for file</returns>
+    public static string JpgaTag(this string filepath, bool isRemove = false)
     {
         string folder = Path.GetFullPath(filepath);
         string file = Path.GetFileName(filepath);
@@ -43,6 +52,11 @@ public static class UsefulFuncsClass
         return Path.Combine(folder, file);
     }
 
+    /// <summary>If a file with the same name exists in the output folder, rename it by adding numbers at the end</summary>
+    /// <param name="filepath">Path to input file</param>
+    /// <param name="folder">Path to output folder</param>
+    /// <param name="ext">Output file extension</param>
+    /// <returns>Path for file</returns>
     public static string IncrFilename(string filepath, string folder, string ext)
     {
         string file = Path.GetFileNameWithoutExtension(filepath);
@@ -56,6 +70,9 @@ public static class UsefulFuncsClass
         return Path.Combine(folder, $"{filename}.{ext}");
     }
 
+    /// <summary>Changes shades of black to transparency</summary>
+    /// <param name="c">Input color</param>
+    /// <returns>Color where black is almost replaced by transparency</returns>
     public static Color ClearBlack(this Color c)
     {
         return Color.FromArgb((int)(255 *
